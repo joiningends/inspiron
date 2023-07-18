3e06ef8aa301409db9bd1fd45e2a00978770e3ac
Arindam Ghosh <aaarindam@gmail.com>
2023-07-18 12:16:53 +0530

    Now added single app.js

--- server/inspiron/app.js
+++ server/inspiron/app.js
@@ -1,10 +1,10 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const morgan = require("morgan");
 const mongoose = require("mongoose");
 const cors = require("cors");
-require("dotenv/config");
 const authJwt = require("./helpers/jwt");
 const errorHandler = require("./helpers/error-handler");
 
@@ -16,7 +16,7 @@ app.options("*", cors());
 app.use(express.json());
 app.use(morgan("tiny"));
 app.use(authJwt());
-//app.use("/public/uploads", express.static(__dirname + "/public/uploads"));
+app.use("/public/uploads", express.static(__dirname + "/public/uploads"));
 app.use(errorHandler);
 
 const appointmentsRoutes = require("./routes/appointments");
@@ -24,21 +24,29 @@ const usersRoutes = require("./routes/users");
 const therapistsRoutes =require("./routes/therapists")
 const assessmentsRoutes =require("./routes/assessments")
 const resetpasswordRoutes = require("./routes/passwordresets")
+const categoriesRoutes = require("./routes/categories");
+const paitentsRoutes = require("./routes/paitents");
+const headingsRoutes = require("./routes/headings");
+const PaymentsRoutes = require("./routes/payments");
 
 const api = process.env.API_URL;
+const PORT = process.env.PORT ||4000;
 
 app.use(`${api}/appointments`, appointmentsRoutes);
 app.use(`${api}/users`, usersRoutes);
 app.use(`${api}/therapists`,  therapistsRoutes);
 app.use(`${api}/assessments`, assessmentsRoutes);
 app.use(`${api}/passwordresets`, resetpasswordRoutes);
+app.use(`${api}/categories`, categoriesRoutes);
+app.use(`${api}/paitents`, paitentsRoutes);
+app.use(`${api}/headings`, headingsRoutes);
+app.use(`${api}/payments`, PaymentsRoutes);
 
 //Database
 mongoose.connect(process.env.CONNECTION_STRING,{
 useNewUrlParser:true,
 useUnifiedTopology: true,
 dbName:'Inspiron'
-
 }).then(() => {
   console.log("Database Connection is ready...");
 })
@@ -46,6 +54,6 @@ dbName:'Inspiron'
   console.log(err);
 });
 //Server
-app.listen(3000, () => {
-  console.log("server is running http://localhost:3000");
+app.listen(PORT, () => {
+  console.log("server is running http://localhost:"+PORT);
 });
