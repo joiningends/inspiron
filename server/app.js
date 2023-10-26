4f63e4c3834fa09d1e45d33fe2ea4a655a33f1b9
Ashutosh Bisht <ashu9872180@gmail.com>
2023-10-26 18:56:10 +0530

    Added build new build folder

--- server/app.js
+++ server/app.js
@@ -1,10 +1,10 @@
+require("dotenv/config");
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const morgan = require("morgan");
 const mongoose = require("mongoose");
 const cors = require("cors");
-require("dotenv/config");
 const authJwt = require("./helpers/jwt");
 const errorHandler = require("./helpers/error-handler");
 const { Appointment } = require("./models/appointment");
