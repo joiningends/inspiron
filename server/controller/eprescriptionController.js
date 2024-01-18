57b29412699606a0c6020dece896e539bad1e8b3
Abhi-shubu <115418689+Abhi-shubu@users.noreply.github.com>
2024-01-18 15:08:28 +0530

    Update eprescriptionController.js

--- server/controller/eprescriptionController.js
+++ server/controller/eprescriptionController.js
@@ -176,7 +176,7 @@ exports.createEPrescription = async (req, res) => {
       // Email the PDF to the user
       sendEmailWithAttachment(user.email, pdfFilePath);
       
-      media_url=`http://13.126.59.21/${pdfFilePath}`
+      media_url=`http://appointments.inspirononline.com/${pdfFilePath}`
       console.log(media_url)
       sendWhatsAppMessageMedia(user.mobile,
    `Attached is your prescription pdf
