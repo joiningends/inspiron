7185e7724afd59d8d55c2e8a7c44bffddc49622b
Abhi-shubu <115418689+Abhi-shubu@users.noreply.github.com>
2024-02-05 18:00:42 +0530

    Delete server directory

--- server/routes/coins.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const coinController = require('../controller/coinController');
-
-
-router.get('/:userId', coinController.getCoinByUserId);
-router.put('/:id', coinController.updateCoinBalance)
-module.exports = router;
