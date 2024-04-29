4468c7bb09e37c0078f977606c04260eabcb6872
Ashutosh Bisht <ashu9872180@gmail.com>
2024-04-29 14:37:51 +0530

    Changes

--- server/routes/therapists.js
+++ server/routes/therapists.js
@@ -24,7 +24,7 @@ const s3 = new aws.S3();
 const upload = multer({
   storage: multerS3({
     s3: s3,
-    bucket: "yasukaimages",
+    bucket: "inspirionimages",
     contentType: multerS3.AUTO_CONTENT_TYPE,
 
     key: function (req, file, cb) {
